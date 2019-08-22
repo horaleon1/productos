effd7b1c191fbbfd30a177d2443f07d3781d4e71
horaleon1 <horaleon1@gmail.com>
2019-08-22 16:00:10 -0500

    "added header"

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
-import Productos from './components/Productos';
-import EditarProducto from './components/EditarProducto';
-import AgregarProducto from './components/AgregarProducto';
-import Producto from './components/Producto';
-
-
-
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Header from "./components/Header";
+import Productos from "./components/Productos";
+import EditarProducto from "./components/EditarProducto";
+import AgregarProducto from "./components/AgregarProducto";
+import Producto from "./components/Producto";
 
 function App() {
   return (
     <Router>
-      <Switch>
-        <Route exact path="/productos/nuevo" component={ AgregarProducto } /> 
-        <Route exact path="/productos" component={ Productos } /> 
-        <Route exact path="/producto" component={ Producto } /> 
-        <Route exact path="/productos/editar/:id" component={ EditarProducto } /> 
-      </Switch>
+      <Header />
+      <main className="container mt-5">
+        <Switch>
+          <Route exact path="/nuevo-producto" component={AgregarProducto} />
+          <Route exact path="/productos" component={Productos} />
+          <Route exact path="/productos/:id" component={Producto} />
+          <Route
+            exact
+            path="/productos/editar/:id"
+            component={EditarProducto}
+          />
+        </Switch>
+      </main>
     </Router>
   );
 }
