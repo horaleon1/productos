d36503e610b283f0947b0f2fc542c8a8c97157a6
horaleon1 <horaleon1@gmail.com>
2019-08-23 17:54:52 -0500

    "added ProductosLista"

--- src/App.js
+++ src/App.js
@@ -1,19 +1,34 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Header from "./components/Header";
 import Productos from "./components/Productos";
 import EditarProducto from "./components/EditarProducto";
 import AgregarProducto from "./components/AgregarProducto";
 import Producto from "./components/Producto";
+import axios from "axios";
 
 function App() {
+  const [productos, guardarProductos] = useState([]);
+
+  useEffect(() => {
+    const consultarApi = async () => {
+      //consultar la api de json-server
+      const resultado = await axios.get("http://localhost:4000/restaurant");
+
+      guardarProductos(resultado.data);
+    };
+    consultarApi();
+  });
+
   return (
     <Router>
       <Header />
       <main className="container mt-5">
         <Switch>
+          <Route exact path="/productos" render = { () => (
+            <Productos productos={ productos }/>
+          )} />
           <Route exact path="/nuevo-producto" component={AgregarProducto} />
-          <Route exact path="/productos" component={Productos} />
           <Route exact path="/productos/:id" component={Producto} />
           <Route
             exact
--- src/components/Productos.js
+++ src/components/Productos.js
@@ -1,11 +1,21 @@
-import React, { Component } from 'react'
+import React, { Fragment } from 'react';
+import ProductosLista from './ProductosLista';
 
-export default class Productos extends Component {
-  render() {
-    return (
-      <div>
-        <h1 className="text-center">  Productos </h1>
-      </div>
-    )
-  }
+
+export default function Productos( {productos} ) {
+  return (
+    <Fragment>
+      <h1 className="text-center"> Productos </h1>
+      <ul className="list-group mt-5">
+          { productos.map(producto => (
+             <ProductosLista 
+             producto={ producto }
+             key={ producto.id }
+             />
+          ))}
+      </ul>
+    </Fragment>
+  )
 }
+
+
